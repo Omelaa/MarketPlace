1e6bd0486d9650ad3885936ea5d88f3aba5a0e39
Omelaa <ostpa28@gmail.com>
2021-03-31 21:06:58 +0300

    homework-1

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,13 @@ $(function(){
 
 
     $('.sofa__inner').slick({
-      
+      dots: true,
+      infinite: true,
+      speed: 1000,
+      fade: true,
+      arrows: false,
     });
+
+     var mixer = mixitup('.products__inner');
 
 })
