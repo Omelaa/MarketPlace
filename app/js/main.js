7cb80225921bed82b9e9ae1853b59dcc522793e1
Omelaa <ostpa28@gmail.com>
2021-04-04 02:30:32 +0300

    home work

--- app/js/main.js
+++ app/js/main.js
@@ -9,6 +9,16 @@ $(function(){
       arrows: false,
     });
 
+    $('.partners__inner').slick({
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:false,
+    focusOnSelect: true,
+    });
+
      var mixer = mixitup('.product-card__inner');
+     var mixer = mixitup('.design__inner');
 
 })
