366014f9e4c21bf935db20e375179929dd3084be
Omelaa <ostpa28@gmail.com>
2021-04-05 01:56:10 +0300

    fix day one

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,40 @@
+
+
 $(function(){
 
-
-    $('.blog__items').slick({
+    $('.trendy__items').slick({
       dots: true,
       infinite: true,
       speed: 1000,
       fade: true,
       arrows: false,
+      autoplay: true,
+      autoplaySpeed: 2000,
     });
 
     $('.partners__inner').slick({
-    slidesToShow: 5,
-    slidesToScroll: 2,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows:false,
-    focusOnSelect: true,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows:false,
+      focusOnSelect: true,
     });
 
-     var mixer = mixitup('.product-card__inner');
-     var mixer = mixitup('.design__inner');
-
-})
+    
+    $('#container-1').mixItUp({
+      selectors: {
+        filter: '.filter'
+      }
+    });
+    
+    
+    $('#сontainer-2').mixItUp({
+      selectors: {
+        filter: '.filter2'
+      }
+    });
+    
+    var mixer = mixitup('#container-1');
+    var mixer = mixitup('#сontainer-2');
+});
